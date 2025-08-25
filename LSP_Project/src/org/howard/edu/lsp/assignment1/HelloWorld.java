ddb687a00f4297b0f866ff85c4f1e35e5d101d7a
tarekdela <66268671+tarekdela@users.noreply.github.com>
2025-08-25 10:57:06 -0400

    Add name

--- LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -2,6 +2,11 @@ package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
+	/**
+	 * Tarek Delavallade
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
